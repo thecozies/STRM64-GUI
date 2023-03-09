4009ea81fb388dbcf27fc7ae0d20739c316ee9d5
thecozies <collinpferguson@gmail.com>
2023-03-09 07:53:20 -0600

    init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: __dirname + '/dist/',
+  },
+  module: {
+    rules: [
+    { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader" ] },
+    { test: /\.tsx?$/, loader: "babel-loader" },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
